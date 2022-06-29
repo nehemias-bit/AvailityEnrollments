97f0fa6e0f53209beb5422b1da190d75d62c585a
nehemias-bit <nemicruz003@gmail.com>
2022-06-29 18:23:04 -0400

    add read file functionality

--- Enrollees/HandleEnrollees.java
+++ Enrollees/HandleEnrollees.java
@@ -1,7 +1,63 @@
 package AvailityEnrollments.Enrollees;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class HandleEnrollees {
 
-  
-  
+  // create treeset and add insurances to hashset
+  HashSet<String> insuranceTree = new HashSet<>();
+  // check list for insurances and create new file with corresponding list
+  // according to insurance
+
+  public HandleEnrollees() {
+
+    // List<String> csvData =
+
+  }
+
+  public void parseCSVData(String file) {
+
+    List<List<String>> csvData = new ArrayList<>();
+
+    String line = "";
+    String splitBy = ",";
+
+    try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+      br.readLine();
+
+      while ((line = br.readLine()) != null) {
+
+        List<String> enrollee = new ArrayList<>();
+        String[] splitEnrollee = new String[1];
+
+        splitEnrollee = line.split(splitBy);
+
+        // System.out.println("splitEnrollee: " + Arrays.asList(splitEnrollee));
+        for (String column : splitEnrollee) {
+          enrollee.add(column);
+        }
+
+        insuranceTree.add(enrollee.get(3).toLowerCase());
+
+        System.out.println("insuranceTree: " + insuranceTree);
+        csvData.add(enrollee);
+        // System.out.println(csvData);
+
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    for (List<String> dataSet : csvData) {
+
+      // writeCSVData(dataSet);
+    }
+  }
+
 }
--- Enrollees/Main.java
+++ Enrollees/Main.java
@@ -3,8 +3,11 @@ package AvailityEnrollments.Enrollees;
 public class Main {
 
   public static void main(String[] args) {
-    
-  }
 
+    HandleEnrollees handleEnrollees = new HandleEnrollees();
+    handleEnrollees
+        .parseCSVData("/Users/nehemiascruz/Availity_Homework/AvailityEnrollments/Enrollees/test_file/enrollee.csv");
+
+  }
 
 }
